7a3310c105a9c84bdca5991d745468bf1d1290fe
Fardin Haque <fardinhaque60@gmail.com>
2025-05-03 16:21:06 -0700

    added stream, settings, and api service components

---
+++ src/app/apiService/api-service.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+  private baseUrl: string = "http://localhost:8000";
+
+  constructor(private http: HttpClient) { }
+
+  postBackendRequest(endpoint: string, payload: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/${endpoint}/`, payload);
+  }
+}
