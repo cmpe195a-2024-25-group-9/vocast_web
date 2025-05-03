3a7c3a62c44b2b6bd356d0009a855a56b37760b1
Fardin Haque <fardinhaque60@gmail.com>
2025-05-03 16:20:43 -0700

    removed boilerplate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SettingsComponent } from './settings/settings.component';
+import { StreamComponent } from './stream/stream.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StreamComponent},
+    { path: 'settings', component: SettingsComponent}
+];
