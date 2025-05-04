675193ebb21e016f42cc01c70b29873819641ebc
Fardin Haque <fardinhaque60@gmail.com>
2025-05-03 17:11:09 -0700

    mic icon example to unmute and make request to py backend

--- src/app/stream/stream.component.ts
+++ src/app/stream/stream.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../apiService/api.service';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stream',
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './stream.component.html',
   styleUrl: './stream.component.css'
 })
 export class StreamComponent {
+  isStreaming: boolean = false;
 
+  constructor(private apiService: ApiService) {}
+
+  toggleStream(): void {
+    this.isStreaming = !this.isStreaming;
+    this.apiService.postBackendRequest('stream', { start: this.isStreaming, IP: "10.0.0.12" }).subscribe({
+      next: (response) => {
+        console.log('Stream toggled successfully:', response);
+      },
+      error: (error) => {
+        console.error('Error toggling stream:', error);
+      }
+    });
+  }
 }
